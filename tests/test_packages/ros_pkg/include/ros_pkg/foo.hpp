c0818ce9ebc98eb815e02c0a26a93a8a9374d3c8
Felix Widmaier <felix.widmaier@tuebingen.mpg.de>
2022-10-21 11:20:39 +0200

    test: Add tests for find_version

---
+++ tests/test_packages/ros_pkg/include/ros_pkg/foo.hpp
@@ -0,0 +1,20 @@
+/**
+ * @brief The Foo class
+ *
+ * This class does foo things.
+ */
+class Foo
+{
+public:
+    /**
+     * @brief Bar the foo.
+     *
+     * @param foo Foo value.
+     *
+     * @return The bared foo value.
+     */
+    int bar(int foo)
+    {
+        return foo;
+    }
+};
